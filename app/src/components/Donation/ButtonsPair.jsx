eaa878e5355a4ea88d6561eea33ca79b23167375
Nax <narendran8155@gmail.com>
2023-10-17 01:01:27 +0800

    highlighting navbar , ancel button enabled

--- app/src/components/Donation/ButtonsPair.jsx
+++ app/src/components/Donation/ButtonsPair.jsx
@@ -1,16 +1,19 @@
 import classes from "../../style/ButtonsPair.module.css";
 
+import {} from 'react-router-dom'
+
 function ButtonsPair(props) {
 
   const {isActive} = props
 
   const handleCancel= ()=>{
     window.location.reload();
+
   }
   
   return (
     <section className={classes.buttons}>
-      <button type="button" className={`btn ${classes.secondary_btn} ${isActive}` }
+      <button type="button" className={`btn ${classes.secondary_btn}` }
       onClick={handleCancel}>
         Cancel
       </button>
